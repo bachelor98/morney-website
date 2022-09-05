74b74287987e9d8eb7f47963f1a09ab99a63a92f
bachelor98 <2596445436@qq.com>
2022-09-06 00:31:15 +0800

    deploy

---
+++ precache-manifest.6d29048ea5af2b3030da903bbf33860a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8947c9a9f8c1493838c",
+    "url": "/morney-website/css/app.90179470.css"
+  },
+  {
+    "revision": "fff0bca6e2b1a53d7bfbd0af3c86fbbf",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "b8947c9a9f8c1493838c",
+    "url": "/morney-website/js/app.a6cf099b.js"
+  },
+  {
+    "revision": "544da331adbe0f6ae012",
+    "url": "/morney-website/js/chunk-vendors.0d31aa17.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
